7b761b70f9a636dab7d13db64f517b8e5b1b4811
Lars Bakker <Lars.bakker@casema.nl>
2023-10-19 11:17:34 +0200

    Made todos link to individual projects (only a couple errors left)

--- src/project.js
+++ src/project.js
@@ -1,43 +1,13 @@
-/* 
-This file appends projects to the project bar. A project is a 
-constructor with a dictionary of todos inside. Upon loading all
-project should be displayed in the project bar with the folder
-image and their name. Upon being clicked this should change to 
-display the content of the todo. A todo will consist of a title
-displayed by default and content displayed on click. This is
-regulated by a boolean variable within each todo.
-
-A project should be appended to the list upon clicking "Create 
-Project", this should then also rerender all project to the project-
-bar. Clicking "Remove Project" should bring up a prompt that asks
-for the name of the project to be deleted. This then deletes it
-from the list and reloads the project-bar.
-
-Since we generated the function for the menu in another JS file
-we need to make an exportable function that activates in home.js.
-This function should execute the above specifics upon clicking the
-corresponding buttons. This means we need 2 functions to be exported.
-One that takes our project list and appends one, then displays it
-in the project bar, and one that deletes a project and displays all
-remaining projects. These function will be named CreateProject and
-DeleteProject respectively. 
-
-The structure of this file has not yet been set on paper but the
-projects should be a constructor that notes its name and an array
-of todos. The todos should be a dictionary with their name, 
-textContent and checkbox,
-*/
-
 import FolderStartingImage from "./assets/images/empty-folder.jpg";
 import FolderHoverImage from "./assets/images/empty-folder-open.png";
-import { CreateTodoScreen, Todo } from "./../src/todo.js";
+import WipeHtml from "./../src/wipeHtml.js";
 
 const projectList = [];
 const projectDivList = [];
 let projectCounter = 0;
 
 class Project {
-	constructor(title="blank", todos={}, projectNumber=undefined) {
+	constructor(title="blank", todos=[], projectNumber=undefined) {
 		this.title = title;
 		this.todos = todos;
 		this.projectNumber = projectNumber;
@@ -55,7 +25,7 @@ export function DisplayProjects() {
 
 let projectNumber = 0;
 
-export function CreateProject(todos={}) {
+export function CreateProject(todos=[]) {
 	if (projectCounter <= 7) {
 		let projectTitle = prompt("What is the project's title?");
 
@@ -106,11 +76,111 @@ export function DeleteProject() {
 	projectCounter--;
 }
 
+let currentProject;
+
 export function AddListeners() {
 	for (let i = 0; i < projectDivList.length; i++) {
 		projectDivList[i].addEventListener("click", function() {
 			CreateTodoScreen();
-			// console.log(projectList[i].todos);
+			currentProject = projectList[i];
+			DisplayTodos();
 		});
 	}
+}
+
+/* 
+
+TODO CODE 
+
+*/
+
+const todoList = [];
+
+export class Todo {
+	constructor(title="blank", textContent="blank", domVersion="blank") {
+		this.title = title;
+		this.textContent = textContent;
+		this.domVersion = domVersion;
+	}
+}
+
+export function CreateTodoScreen() {
+    const functionBar = document.getElementById("function-bar");
+    const functionList = [];
+
+    WipeHtml();
+
+    let createTodo = document.createElement("p");
+    createTodo.classList.add("function");
+    createTodo.classList.add("createTodo");
+    createTodo.textContent = "Create Todo";
+
+    createTodo.addEventListener("click", function() {
+        CreateTodo();
+		CreateTodoScreen();
+        DisplayTodos();
+    });
+
+    functionList.push(createTodo);
+
+    functionBar.appendChild(createTodo);
+
+    let deleteTodo = document.createElement("p");
+    deleteTodo.classList.add("function");
+    deleteTodo.classList.add("deleteTodo");
+    deleteTodo.textContent = "Delete Todo";
+
+    deleteTodo.addEventListener("click", function() {
+        // grab 
+    });
+
+    functionList.push(deleteTodo);
+
+    functionBar.style.gap = String(((100 / functionList.length) / functionList.length) + "%");
+
+    functionBar.appendChild(deleteTodo);
+}
+
+let todoNumber = 0;
+
+function CreateTodo() {
+	let todoTitle = prompt("What is the todo's title?");
+    let todoText = prompt("What is the todo's text content?");
+    let domVersion = `<div class="todo ${String(`todo${todoNumber}`)}><h3 class="todoTitle>${todoTitle}</h3><p class="todoText">${todoText}</p></div>"`;
+
+    todoNumber++;
+
+    let todo = new Todo(todoTitle, todoText, domVersion);
+	currentProject.todos.push(todo);
+
+	todoList.push(todo);
+}
+
+function DeleteTodo() {
+    //
+}
+
+function DisplayTodos() {
+    let mainField = document.getElementById("main-field");
+
+    for (let i = 0; i < todoList.length; i++) {
+		if (currentProject.todos.length > 0) {
+			const todoDiv = document.createElement("div");
+			todoDiv.classList.add("todo", String(`todo${todoNumber}`));
+
+			let todoTitleElement = document.createElement("h3");
+			todoTitleElement.classList.add("todoTitle");
+			todoTitleElement.textContent = currentProject.todos[i].title;
+
+			todoDiv.appendChild(todoTitleElement);
+
+			let todoTextElement = document.createElement("p");
+			todoTextElement.classList.add("todoText");
+			todoTextElement.textContent = currentProject.todos[i].textContent;
+
+			todoDiv.appendChild(todoTextElement);
+
+			mainField.appendChild(todoDiv);
+		}
+    }
 }
